2717bb16cd74dd898223f701079cd919dee592fd
Bernhard <bernton96@gmail.com>
2022-04-07 09:43:47 +0200

    initial setup for calibration class

---
+++ calibration.py
@@ -0,0 +1,18 @@
+import numpy as np
+import cv2
+import glob
+import matplotlib.pyplot as plt
+
+
+
+class Calibration:
+    def __init__(self):
+        pass
+
+# some more functions
+
+
+
+
+if __name == "__main__":
+    # test code
