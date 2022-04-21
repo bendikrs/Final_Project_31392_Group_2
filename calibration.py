8586c108d07eb9089a07ecf47e0eb1a68c4ec942
Bernhard <bernton96@gmail.com>
2022-04-21 08:59:11 +0200

    fixed time issiue and cleaned up the code

--- calibration.py
+++ calibration.py
@@ -10,18 +10,23 @@ class Calibration:
         # setup for the calibration images
         self.PATH = img_path
         self.pattern_size = (nb_vertical, nb_horizontal)
-
-        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-        self.objp = np.zeros((nb_horizontal*nb_vertical,3), np.float32)
-        self.objp[:,:2] = np.mgrid[0:nb_vertical,0:nb_horizontal].T.reshape(-1,2)
-
-        # Arrays to store object points and image points from all the images.
-        self.objpoints = [] # 3d point in real world space
-        self.imgpoints = [] # 2d points in image plane.
-        self.cameramatrixLeft = [] # camera matrix for left camera
-        self.cameramatrixRight = [] # camera matrix for right camera
+        
+        # for distorsion
+        self.mtx = None
+        self.dist = None
+        self.roi = None
+        self.newcameramtx = None
 
     def calibrate(self):
+        
+        # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+        objp = np.zeros((self.pattern_size[0]*self.pattern_size[1],3), np.float32)
+        objp[:,:2] = np.mgrid[0:self.pattern_size[0],0:self.pattern_size[1]].T.reshape(-1,2)
+
+        # Arrays to store object points and image points from all the images.
+        objpoints = [] # 3d point in real world space
+        imgpoints = [] # 2d points in image plane.
+        
         # Load the images
         images = glob.glob(self.PATH)
         assert images
@@ -36,11 +41,11 @@ class Calibration:
 
             # If found, add object points, image points (after refining them)
             if ret == True:
-                self.objpoints.append(self.objp)
-                self.imgpoints.append(corners)
+                objpoints.append(objp)
+                imgpoints.append(corners)
         
         h, w = img.shape[:2]
-        ret, self.mtx, self.dist, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, self.imgpoints, gray.shape[::-1], None, None)
+        ret, self.mtx, self.dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
         self.newcameramtx, self.roi = cv2.getOptimalNewCameraMatrix(self.mtx,self.dist,(w,h),1,(w,h))
     
     def distort_img(self, img, Crop = False):
@@ -59,9 +64,11 @@ class Calibration:
 if __name__ == "__main__":
     # test code
 
-    calitest = Calibration("data/calibration_images/*.png", 9, 6)
-
+    calitest = Calibration("data/calibration_images/*.png", 6, 9)
+    print("1")
     calitest.calibrate()
-
+    print("2")
+    cv2.imshow("distorted", cv2.imread("data/calibration_images/left-0001.png")
     dst = calitest.distort_img(cv2.imread("data/calibration_images/left-0001.png"), True)
+    print("3")
     cv2.imshow("undistorted", dst)
