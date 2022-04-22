305685a4dbc1ad39271850435cd851dbfbce96aa
Bernhard <bernton96@gmail.com>
2022-04-22 12:10:00 +0200

    Started working on rectification

--- calibration.py
+++ calibration.py
@@ -11,24 +11,53 @@ class Calibration:
         self.PATH = img_path
         self.pattern_size = (nb_vertical, nb_horizontal)
         
+        self.img_shape = None
         # for distorsion
-        self.mtx = None
-        self.dist = None
-        self.roi = None
-        self.newcameramtx = None
+        self.objpoints = [] 
 
-    def calibrate(self,Show = False):
+        self.left_mtx = None
+        self.left_dist = None
+        self.left_roi = None
+        self.left_cameramtx = None
+        self.left_imgpoints = None
+
+        self.right_mtx = None
+        self.right_dist = None
+        self.right_roi = None
+        self.right_cameramtx = None
+        self.right_imgpoints = None
+        
+        self.left_map = None
+        self.right_map = None
+
+
+    def calibrate(self, Show = False):
+        
+        #calibrate left camera
+        ind = self.PATH.find("*")
+        path = self.PATH[0:ind] + "left" + self.PATH[ind:]
+        print(path)
+        self.left_mtx, self.left_dist, self.left_roi, self.left_cameramtx, self.left_imgpoints = self._calibrate(path,Show)
+
+        #calibrate the right camera
+        path = self.PATH[0:ind] + "right" + self.PATH[ind:]
+        print(path)
+        self.right_mtx, self.left_dist, self.left_roi, self.left_cameramtx, self.right_imgpoints = self._calibrate(path,Show)
+        
+        self._rectify(self.img_shape,Show)
+
+    def _calibrate(self, Path, Show):
         
         # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
         objp = np.zeros((self.pattern_size[0]*self.pattern_size[1],3), np.float32)
         objp[:,:2] = np.mgrid[0:self.pattern_size[0],0:self.pattern_size[1]].T.reshape(-1,2)
 
         # Arrays to store object points and image points from all the images.
-        objpoints = [] # 3d point in real world space
+        self.objpoints = [] # 3d point in real world space
         imgpoints = [] # 2d points in image plane.
         
         # Load the images
-        images = glob.glob(self.PATH)
+        images = glob.glob(Path)
         assert images
         
         # perform calibration for left images only
@@ -41,7 +70,7 @@ class Calibration:
 
             # If found, add object points, image points (after refining them)
             if ret == True:
-                objpoints.append(objp)
+                self.objpoints.append(objp)
                 imgpoints.append(corners)
 
         if Show == True:
@@ -51,37 +80,80 @@ class Calibration:
                                                 
         
         h, w = img.shape[:2]
-        ret, self.mtx, self.dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-        self.newcameramtx, self.roi = cv2.getOptimalNewCameraMatrix(self.mtx,self.dist,(w,h),0,(w,h))
+        self.img_shape = gray.shape[::-1] 
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(self.objpoints, imgpoints, self.img_shape, None, None)
+        newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),0,(w,h))
     
+        return mtx, dist, roi, newcameramtx, imgpoints
+
     def distort_img(self, img, Crop = False):
 
         #get undistortet image
-        dst = cv2.undistort(img, self.mtx, self.dist, None, self.newcameramtx)
+        dst = cv2.undistort(img, self.left_mtx, self.left_dist, None, self.left_cameramtx)
 
         #crop if needed
         if Crop == True:
-            x,y,w,h = self.roi
+            x,y,w,h = self.left_roi
             dst = dst[y:y+h, x:x+w]
         return dst
 
-    def rectify(self):
-        pass
+    def _rectify(self, dims, Show = False):
+        flags = 0
+        flags |= cv2.CALIB_FIX_INTRINSIC
+        # flags |= cv2.CALIB_FIX_PRINCIPAL_POINT
+        flags |= cv2.CALIB_USE_INTRINSIC_GUESS
+        flags |= cv2.CALIB_FIX_FOCAL_LENGTH
+        # flags |= cv2.CALIB_FIX_ASPECT_RATIO
+        flags |= cv2.CALIB_ZERO_TANGENT_DIST
+        # flags |= cv2.CALIB_RATIONAL_MODEL
+        # flags |= cv2.CALIB_SAME_FOCAL_LENGTH
+        # flags |= cv2.CALIB_FIX_K3
+        # flags |= cv2.CALIB_FIX_K4
+        # flags |= cv2.CALIB_FIX_K5
+        stereocalib_criteria = (cv2.TERM_CRITERIA_MAX_ITER +                                                                                                                                                cv2.TERM_CRITERIA_EPS, 100, 1e-5)
+                                                             
+        ret, M1, d1, M2, d2, R, T, E, F = cv2.stereoCalibrate(self.objpoints, self.left_imgpoints,
+                                                              self.right_imgpoints, self.left_mtx, self.left_dist, 
+                                                              self.right_mtx,self.right_dist, dims,
+                                                              criteria=stereocalib_criteria, flags=flags)
+        
+        R1, R2, P1, P2, Q, roi1, roi2 = cv2.stereoRectify(M1,d1,M2,d2,dims,R,T) 
+        
+        self.left_map = cv2.initUndistortRectifyMap(M1,d1,R1,P1,dims,0)
+        self.right_map = cv2.initUndistortRectifyMap(M2,d2,R2,P2,dims,0)
 
+
+    def drawlines(self, img1,img2,lines,pts1,pts2):
+        ''' img1 - image on which we draw the epilines for the points in img2
+            lines - corresponding epilines '''
+        r,c = img1.shape
+        img1 = cv2.cvtColor(img1,cv2.COLOR_GRAY2BGR)
+        img2 = cv2.cvtColor(img2,cv2.COLOR_GRAY2BGR)
+        for r,pt1,pt2 in zip(lines,pts1,pts2):
+            color = tuple(np.random.randint(0,255,3).tolist())
+            x0,y0 = map(int, [0, -r[2]/r[1] ])
+            x1,y1 = map(int, [c, -(r[2]+r[0]*c)/r[1] ])
+            img1 = cv2.line(img1, (x0,y0), (x1,y1), color,2)
+            img1 = cv2.circle(img1,tuple(pt1),5,color,-1)
+            img2 = cv2.circle(img2,tuple(pt2),5,color,-1)
+        return img1,img2
+        
+    def left_remap(self,img):
+        return cv2.remap(img,self.left_map[0],self.left_map[1],cv2.INTER_NEAREST, cv2.BORDER_CONSTANT)
+
+
+    def right_remap(self,img):
+        pass
 
 if __name__ == "__main__":
     # test code
 
-    left = Calibration("data/calibration_images/left*.png", 6, 9)
-    right = Calibration("data/calibration_images/right*.png", 6, 9)
+    cali = Calibration("data/calibration_images/*.png", 6, 9)
     print("1")
-    left.calibrate(Show = True)
-    right.calibrate(Show = True)
+    cali.calibrate()
     print("2")
-    cv2.imshow("left distorted", cv2.imread("data/calibration_images/left-0001.png"))
-    cv2.imshow("right distorted", cv2.imread("data/calibration_images/right-0001.png"))
-    cv2.waitKey(0)
-    dst = left.distort_img(cv2.imread("data/calibration_images/left-0001.png"), True)
+    cv2.imshow("before", cv2.imread("data/calibration_images/left-0001.png"))
+    dst = cali.left_remap(cv2.imread("data/calibration_images/left-0001.png"))
     print("3")
-    cv2.imshow("undistorted", dst)
+    cv2.imshow("after", dst)
     cv2.waitKey(0)
